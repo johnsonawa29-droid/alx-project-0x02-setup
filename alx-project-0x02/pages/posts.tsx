1c508cb308d35520d65b3324bec1df2e2c799186
johnson <johnsonawa29@gmail.com>
2025-09-27 20:25:39 +0100

    yes

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <>
+      <Header />
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Posts Page</h1>
+        <p>This is where posts will be displayed.</p>
+      </div>
+    </>
+  );
+}
