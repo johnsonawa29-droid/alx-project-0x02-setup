93f38995e5eb1ae17147f612cc2c811deebd9866
johnson <johnsonawa29@gmail.com>
2025-09-27 20:46:30 +0100

    yes

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -26,3 +26,15 @@ export interface PostProps {
   title: string;
   body: string;
 }
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+}
