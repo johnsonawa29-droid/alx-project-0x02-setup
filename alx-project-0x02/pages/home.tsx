a2ea8a9cb242bddefe19827212bb7c194fd5c993
johnson <johnsonawa29@gmail.com>
2025-09-27 19:10:30 +0100

    yes

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,4 @@
-export default function HomePage() {
-  return <h1 className="p-8 text-2xl">This is the Home page</h1>;
-}
-import Card from "../components/common/Card";
+import Card from "@/components/common/Card";
 
 export default function HomePage() {
   return (
@@ -9,11 +6,11 @@ export default function HomePage() {
       <h1 className="text-2xl font-bold mb-4">Home Page</h1>
       <Card
         title="First Card"
-        description="This is the description for the first card."
+        content="This is the content for the first card."
       />
       <Card
         title="Second Card"
-        description="Here is some more content for the second card."
+        content="Here is some more content for the second card."
       />
     </div>
   );
