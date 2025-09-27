d133922e9ac0d19097c2717fbee81ccf89152eec
johnson <johnsonawa29@gmail.com>
2025-09-27 20:37:41 +0100

    yes

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -52,3 +52,44 @@ export async function getStaticProps() {
     },
   };
 }
+
+import Header from "@/components/layout/Header";
+import PostCard from "@/components/common/PostCard";
+import { type PostProps } from "@/interfaces";
+
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+export default function PostsPage({ posts }: PostsPageProps) {
+  return (
+    <>
+      <Header />
+      <div className="p-8 space-y-4">
+        <h1 className="text-2xl font-bold mb-4">Posts Page</h1>
+        <div className="grid gap-4">
+          {posts.map((post) => (
+            <PostCard
+              key={post.id}
+              id={post.id}
+              userId={post.userId}
+              title={post.title}
+              body={post.body}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+  const posts: PostProps[] = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
